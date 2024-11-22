249aa1dbf6f2c67417b9e196331fdb4b871f00d3
midhulakp <115697790+midhulakp@users.noreply.github.com>
2024-11-22 03:20:19 +0000

    changes made

--- .devcontainer/JAVASCRIPT/Rest_and_Spread.js
+++ .devcontainer/JAVASCRIPT/Rest_and_Spread.js
@@ -1,12 +1,18 @@
+//REST operator
+//used only in functions,it binds the rest of the properties in an array form
+
 function mydetails(name, age, ...restOfDetails) {
     console.log(name);
     console.log(age);
     console.log(restOfDetails);
   
-  }
+  }   
   
   mydetails("john",34,"library","dogs","LA")
   
+
+  //Spread operator
+  //unpacking the values
   
   let a = [1, 2, 3, 4, 5]
   
